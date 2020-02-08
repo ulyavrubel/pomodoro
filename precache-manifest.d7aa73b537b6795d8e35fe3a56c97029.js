d2781ba6ff6fe6bd3b359ce1df5eb3f9d6341a36
ulyavrubel <uvrubel@gmail.com>
2020-02-08 19:04:42 +0800

    Updates

---
+++ precache-manifest.d7aa73b537b6795d8e35fe3a56c97029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e81620f7545baa15304d757afc7cb4a",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "89d1a1c9b45227f5b88c",
+    "url": "/pomodoro/static/css/main.0487da74.chunk.css"
+  },
+  {
+    "revision": "b5a9ddc18f1c8371d943",
+    "url": "/pomodoro/static/js/2.23d91071.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro/static/js/2.23d91071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d1a1c9b45227f5b88c",
+    "url": "/pomodoro/static/js/main.21b1eeae.chunk.js"
+  },
+  {
+    "revision": "f7605364f192b19e0c94",
+    "url": "/pomodoro/static/js/runtime-main.a351b351.js"
+  }
+]);
